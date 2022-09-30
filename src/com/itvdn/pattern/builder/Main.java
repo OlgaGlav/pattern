398e8d01c3360e64f75635f189bf25d82a40bf20
Olga <pin007@tut.by>
2022-09-30 11:57:22 +0300

    with @Builder lombok

--- src/com/itvdn/pattern/builder/Main.java
+++ src/com/itvdn/pattern/builder/Main.java
@@ -6,13 +6,15 @@ import com.itvdn.pattern.builder.pizza.component.Meat;
 import com.itvdn.pattern.builder.pizza.component.Sauce;
 import com.itvdn.pattern.builder.pizza.pizza.Pizza;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
-        Pizza pizza = new Pizza.PizzaBuilder(
-                Dough.THICK, Sauce.RED)
-                .setCheese(Cheese.CHEDDER, Cheese.PARMESAN)
-                .setMeat(Meat.SALAMI, Meat.SMOKED_CHICKEN)
+        Pizza pizza = Pizza.builder()
+                .dough(Dough.THICK)
+                .sauce(Sauce.RED)
+                .cheeses(Arrays.asList(Cheese.CHEDDER, Cheese.PARMESAN))
+                .meats(Arrays.asList(Meat.SALAMI, Meat.SMOKED_CHICKEN))
                 .build();
 
         System.out.println(pizza);
--- src/com/itvdn/pattern/builder/pizza/pizza/Pizza.java
+++ src/com/itvdn/pattern/builder/pizza/pizza/Pizza.java
@@ -1,62 +1,20 @@
 package com.itvdn.pattern.builder.pizza.pizza;
 
 import com.itvdn.pattern.builder.pizza.component.*;
+import lombok.Builder;
 import lombok.Getter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+
 @Getter
+@Builder
 public class Pizza {
     private final Dough dough;
     private final Sauce sauce;
     private final List<Cheese> cheeses;
     private final List<Meat> meats;
     private final List<Vegetables> vegetables;
-
-    private Pizza(PizzaBuilder builder) {
-        this.dough = builder.dough;
-        this.sauce = builder.sauce;
-        this.cheeses = builder.cheese;
-        this.meats = builder.meat;
-        this.vegetables = builder.vegetables;
-    }
-
-    public static class PizzaBuilder {
-        private final Dough dough;
-        private final Sauce sauce;
-        private List<Cheese> cheese;
-        private List<Meat> meat;
-        private List<Vegetables> vegetables;
-
-        public PizzaBuilder(Dough dough, Sauce sauce) {
-            this.dough = dough;
-            this.sauce = sauce;
-        }
-
-        public PizzaBuilder setCheese(Cheese... cheeses) {
-            this.cheese = new ArrayList<>();
-            this.cheese.addAll(Arrays.asList(cheeses));
-            return this;
-        }
-
-        public PizzaBuilder setMeat(Meat... meats) {
-            this.meat = new ArrayList<>();
-            this.meat.addAll(Arrays.asList(meats));
-            return this;
-        }
-
-        public PizzaBuilder setVegetables(Vegetables... vegetables) {
-            this.vegetables = new ArrayList<>();
-            this.vegetables.addAll(Arrays.asList(vegetables));
-            return this;
-        }
-
-        public Pizza build() {
-            return new Pizza(this);
-        }
-    }
 
     @Override
     public String toString() {
